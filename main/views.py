eb0e3ff0ac19decd7ceada316ebd420776cb9d32
david <dongkpak@gmail.com>
2022-04-15 23:25:39 -0400

    11

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import get_object_or_404, render, redirect ## 추가된 부분
+
+def home(request): 
+   
+    return render(request, 'home.html')
