d04c4b4132e2f0806bc6293327fe8c000cf130bc
david <dongkpak@gmail.com>
2022-05-04 22:29:04 -0400

    s

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from main import views
  
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^board/question_create/$', views.question_create, name='question_create'), ## 추가 ##    
+    #url(r'^board/question_create/$', views.question_create, name='question_create'), ## 추가 ##    
 
   
 ] 
