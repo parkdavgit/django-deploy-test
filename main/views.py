5d1387227e1f368431d5b6c9806b876fe0b2c4e6
david <dongkpak@gmail.com>
2022-05-04 22:37:59 -0400

    d

--- main/views.py
+++ main/views.py
@@ -32,17 +32,4 @@ def home(request):
 
 
 
-def question_create(request):
-    if request.method == 'POST':
-        form = NewQuestionForm(request.POST)
-        if form.is_valid():
-            question = form.save(commit=False)
-            question.create_at = timezone.now()
-
-            question.author = request.user         ### 추가 ####
-
-            question.save()
-            return redirect('home')
-    else:
-        form = NewQuestionForm()
-    return render(request, 'question_create.html', {'form':form})    
+ 
