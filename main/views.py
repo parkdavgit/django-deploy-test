1dcfef848a6f79b814abe9b9976769283924109e
david <dongkpak@gmail.com>
2022-05-05 22:46:04 -0400

    jJ

--- main/views.py
+++ main/views.py
@@ -2,29 +2,13 @@
 
 # Create your views here.
 from django.shortcuts import get_object_or_404, render, redirect ## 추가된 부분
-#from .models import Question ## 추가된 부분
-#from django.utils import timezone
-#from .forms import NewQuestionForm 
-#from django.contrib import messages
+ 
 
 def home(request): 
    
     return render(request, 'home.html')
 
-#def home(request): 
-    #question_list = Question.objects.order_by('subject') 
-    # 입력 파라미터
-    #page = request.GET.get('page','1')
-     
-    # 페이징처리
-    #paginator = Paginator(question_list, 7)
-    #page_obj = paginator.get_page(page)
-   
-    #page_obj = 1
-    #return render(request, 'home.html',{'question_list':page_obj})
-    #return render(request, 'home.html')
-
-
+ 
 
 
 
