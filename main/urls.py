6fd1cd8eb34e129443904c42f8288eee659d372e
david <dongkpak@gmail.com>
2022-05-05 23:18:03 -0400

    aa

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,8 @@ from main import views
  
  
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    #url(r'^board/question_create/$', views.question_create, name='question_create'), ## 추가 ##    
+    url(r'^$', views.index, name='index'),
+    
 
   
 ] 
--- main/views.py
+++ main/views.py
@@ -1,18 +1,13 @@
-#from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import get_object_or_404, render, redirect ## 추가된 부분
+from django.shortcuts import render
  
 
-def home(request): 
-   
-    return render(request, 'home.html')
+def index(request): 
+    return render(request, 'index.html')
 
  
 
 
-
-
+ 
 
 
 
