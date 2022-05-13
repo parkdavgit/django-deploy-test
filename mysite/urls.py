bdb4d22b6b3fa563c1734b5703bd2185aad81419
david <dongkpak@gmail.com>
2022-05-13 15:28:00 -0400

    t

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+ 
+
+
 urlpatterns = [
     
     path('', include('main.urls')), ## 추가된 부분
