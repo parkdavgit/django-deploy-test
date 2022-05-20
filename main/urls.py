109e742aef7655262d11e54b51e223dcca312d0f
david <dongkpak@gmail.com>
2022-05-19 20:32:37 -0400

    b

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from main import views
  
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^board/question_create/$', views.question_create, name='question_create'),
 ] 
 
  
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect ## 추가된 부분
 from .models import Question ## 추가된 부분
 from django.utils import timezone
-#from .forms import NewQuestionForm, AnswerForm 
+from .forms import NewQuestionForm, AnswerForm 
 from django.core.paginator import Paginator 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -15,3 +15,18 @@ def index(request):
     paginator = Paginator(question_list, 7)
     page_obj = paginator.get_page(page)
     return render(request, 'index.html',{'question_list':page_obj})
+
+def question_create(request):
+    if request.method == 'POST':
+        form = NewQuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_at = timezone.now()
+
+            question.author = request.user         ### 추가 ####
+
+            question.save()
+            return redirect('index')
+    else:
+        form = NewQuestionForm()
+    return render(request, 'question_create.html', {'form':form})
