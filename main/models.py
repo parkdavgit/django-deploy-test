c5a6df2658849fe02d7090e5c3941c9874ff17d7
david <dongkpak@gmail.com>
2022-05-22 23:06:04 -0400

    e

--- main/models.py
+++ main/models.py
@@ -14,14 +14,14 @@ class Question(models.Model):
         return self.subject
 
   
-class Quest(models.Model):
-    subject = models.CharField(max_length=100)
-    content = models.TextField()
-    #create_at = models.DateTimeField(auto_now_add=True)
-    #modify_at = models.DateTimeField(null=True, blank=True)
-    #author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question') ## 수정 ##
-    #voter = models.ManyToManyField(User, related_name='voter_question')  ## 추가 ##
- 
-    def __str__(self):
-        return self.subject
+
+
+class Item(models.Model):
+	 
+	title= models.CharField(max_length=100)
+	price= models.FloatField()
+	offer= models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.title        
  
