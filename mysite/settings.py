510f40cb1972c182dda5ff3cb62c6b638d16d4d2
david <dongkpak@gmail.com>
2022-05-24 20:05:58 -0400

    q

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', ## 추가된 부분
-        'NAME': 'test_db', ## 추가된 부분
-        'USER': 'pak', ## 추가된 부분
-        'PASSWORD':'1234', ## 추가된 부분 
+        'NAME': 'ubbs', ## 추가된 부분
+        'USER': 'park', ## 추가된 부분
+        'PASSWORD':'4444', ## 추가된 부분 
         'HOST':'18.188.208.156' ## 추가된 부분
     }
 }
