510b03f1f5e1f956f6433574b9ab9db6f5035bf0
david <dongkpak@gmail.com>
2022-05-27 22:31:45 -0400

    L

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 
 from django.contrib import admin
-#from .models import Question
+from .models import Question, Item 
 
 # Register your models here.
 
-#admin.site.register(Question) 
+admin.site.register(Question) 
+admin.site.register(Item)
