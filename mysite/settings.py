2482b3dd4e55399d5f6a4e5a37f06f70e4e1ea1b
david <dongkpak@gmail.com>
2022-05-28 10:29:41 -0400

    r

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    #'main',
     'account',
-    'main.apps.MainConfig',
+    'mysite.main',
 ]
 
 MIDDLEWARE = [
