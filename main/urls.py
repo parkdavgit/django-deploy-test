ba5bdfbd8a0ecfb7e1d2e0d81cb71d9c8fbe8b66
david <dongkpak@gmail.com>
2022-05-30 15:05:54 -0400

    K

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^board/(?P<question_id>\d+)/$', views.question_detail, name='question_detail'),
     url(r'^board/question_create/$', views.question_create, name='question_create'), ## 추가 ##
-     
+    url(r'^board/question_modify/(?P<question_id>\d+)/$', views.question_modify, name='question_modify'),
+    url(r'^board/question_delete/(?P<question_id>\d+)/$', views.question_delete, name='question_delete'),
+
+    
 ] 
 
  
