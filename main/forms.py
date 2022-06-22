aefe7d0da00c397d668eb3d26a9673aed61f9da1
david <dongkpak@gmail.com>
2022-06-22 19:59:41 -0400

    this directory

--- main/forms.py
+++ main/forms.py
@@ -21,14 +21,16 @@ class NewItemForm(forms.ModelForm):
 ## 해당 부분에 form에서 입력 받을 속성을 결정합니다. models.py와 동일하게 설정하면 됩니다. ##
     title = forms.CharField(
         max_length=200,
-        label='제목')
+        label='Item')
     price = forms.FloatField(
         label='Price'
     )
-
+    offer = forms.BooleanField(
+        label='offer'
+    )
 
 ## 어떤 모델을 사용하고 모델에서 사용할 속성을 지정 합니다 ##
     class Meta:
         model = Item
-        fields = ['title', 'price']
+        fields = ['title', 'price', 'offer']
  
