405aaae7728d7f9b7100f98fff8ddc18ceb4c38d
david <dongkpak@gmail.com>
2022-07-20 18:39:06 -0400

    this directory

--- ecpoll/urls.py
+++ ecpoll/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from main import views 
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    #url(r'^$', views.home, name='home'),
+    url(r'^$', views.index, name='index'),
    ] 
