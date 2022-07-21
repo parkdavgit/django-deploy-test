e0b98fb36a8f640e3518fb7cac33a77cbc391b31
david <dongkpak@gmail.com>
2022-07-21 10:38:53 -0400

    this directory

--- ecpoll/models.py
+++ ecpoll/models.py
@@ -1,3 +1,50 @@
-from django.db import models
+#from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+import secrets
+
+class Poll(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    pub_date = models.DateTimeField(default=timezone.now)
+    active = models.BooleanField(default=True)
+
+    def user_can_vote(self, user):
+        """ 
+        Return False if user already voted
+        """
+        user_votes = user.vote_set.all()
+        qs = user_votes.filter(poll=self)
+        if qs.exists():
+            return False
+        return True
+
+    @property
+    def get_vote_count(self):
+        return self.vote_set.count()
+
+    def get_result_dict(self):
+        res = []
+        for choice in self.choice_set.all():
+            d = {}
+            alert_class = ['primary', 'secondary', 'success',
+                           'danger', 'dark', 'warning', 'info']
+
+            d['alert_class'] = secrets.choice(alert_class)
+            d['text'] = choice.choice_text
+            d['num_votes'] = choice.get_vote_count
+            if not self.get_vote_count:
+                d['percentage'] = 0
+            else:
+                d['percentage'] = (choice.get_vote_count /
+                                   self.get_vote_count)*100
+
+            res.append(d)
+        return res
+
+    def __str__(self):
+        return self.text
+ 
