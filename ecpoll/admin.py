5cf14acec9db018bd8760764e655d88883a3d10c
david <dongkpak@gmail.com>
2022-07-22 20:49:22 -0400

    just one file

--- ecpoll/admin.py
+++ ecpoll/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+#from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Poll
+# Register your models here.
+
+admin.site.register(Poll)
